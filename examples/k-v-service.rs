0adb18518d721d8091e6ae5002b3053eda97057f
Lisza <lisza.zeidler@mailbox.tu-dresden.de>
2022-05-26 12:11:02 +0200

    simple k-v-application running

--- examples/k-v-service.rs
+++ examples/k-v-service.rs
@@ -1,15 +1,28 @@
 mod utils;
-use std::collections::{BTreeMap, HashMap};
+
+use std::collections::{BTreeMap};
 use std::os::unix::io::AsRawFd;
-use std::fmt::Write;
+use std::str;
 
 use log::debug;
-use smoltcp::ohua_util::store::{Store, Message, RequestMsg};
+use smoltcp::ohua_util::store::Store;
 use smoltcp::iface::{InterfaceBuilder, NeighborCache};
-use smoltcp::phy::{wait as phy_wait, Medium, Device, TunTapInterface, Loopback};
+use smoltcp::phy::{wait as phy_wait, Medium, Device, TunTapInterface};
 use smoltcp::socket::{TcpSocket, TcpSocketBuffer};
 use smoltcp::time::Instant;
 use smoltcp::wire::{EthernetAddress, IpAddress, IpCidr};
+
+fn process_octets(octets:&mut [u8]) -> (usize, Vec<u8>) {
+    let recvd_len = octets.len();
+    let data = octets.to_owned();
+    if !data.is_empty(){
+        debug!(
+            "tcp:6970 recv data: {:?}",
+            str::from_utf8(data.as_ref()).unwrap_or("(invalid utf8)")
+        );
+    }
+    (recvd_len, data)
+}
 
 fn main() {
     println!(
@@ -27,43 +40,27 @@ fn main() {
      \/__/         \/__/         \/__/         \/__/                  \|__|
 "#
     );
-    // 1. Do something with Store
-    //let mut some_table = HashMap::new();
-    //some_table.insert("k1", "v1");
     let mut store = Store::default();
-    let test_msg = Message::Read(RequestMsg {table: "sometable".to_string(), key: "somekey".to_string() });
-
-    // 2. Get the simple hello world server running
-    // 3. simplify as far as possible i.e. use as few smolTCP structs 'n helpers
-    /* Leave out command line configuration for now
 
 
-    let (mut opts, mut free) = utils::create_options();
-    utils::add_tuntap_options(&mut opts, &mut free);
-    utils::add_middleware_options(&mut opts, &mut free);
-
-    let mut matches = utils::parse_options(&opts, free);
-    let device = utils::parse_tuntap_options(&mut matches);
-    let fd = device.as_raw_fd();
-    let device = utils::parse_middleware_options(&mut matches, device, /*loopback=*/ false);
-      */
     let tcp_rx_buffer = TcpSocketBuffer::new(vec![0; 64]);
     let tcp_tx_buffer = TcpSocketBuffer::new(vec![0; 128]);
     let tcp_socket = TcpSocket::new(tcp_rx_buffer, tcp_tx_buffer);
 
-    /*For now I use loopback as interface*/
+
     let device = TunTapInterface::new("tap0", Medium::Ethernet).unwrap();
     let fd = device.as_raw_fd();
     let neighbor_cache = NeighborCache::new(BTreeMap::new());
+    let ethernet_addr = EthernetAddress([0x02, 0x00, 0x00, 0x00, 0x00, 0x01]);
     let ip_addrs = [
-        IpCidr::new(IpAddress::v4(192, 168, 211, 117), 8)
+        IpCidr::new(IpAddress::v4(192, 168, 69, 1), 24)
     ];
 
     let medium = device.capabilities().medium;
     let mut builder = InterfaceBuilder::new(device, vec![]).ip_addrs(ip_addrs);
     if medium == Medium::Ethernet {
         builder = builder
-            .hardware_addr(EthernetAddress::default().into())
+            .hardware_addr(ethernet_addr.into())
             .neighbor_cache(neighbor_cache);
     }
     let mut iface = builder.finalize();
@@ -78,25 +75,27 @@ fn main() {
                 debug!("poll error: {}", e);
             }
         }
-        println!("Made it through polling");
-        // tcp: respond "hello"
+
         let socket = iface.get_socket::<TcpSocket>(tcp_handle);
         if !socket.is_open() {
             socket.listen(6969).unwrap();
         }
-        if socket.is_open(){
-            println!("socket 6969 is listening");
-        }
 
-        if socket.can_send() {
-            println!("tcp:6969 send greeting");
-            writeln!(socket, "hello").unwrap();
-            println!("tcp:6969 close");
+        if socket.may_recv() {
+            let input = socket.recv(process_octets).unwrap();
+            if socket.can_send() && !input.is_empty() {
+                debug!(
+                    "tcp:6969 send data: {:?}",
+                    str::from_utf8(input.as_ref()).unwrap_or("(invalid utf8)")
+                );
+                let outbytes = store.handle_message(&input);
+                socket.send_slice(&outbytes[..]).unwrap();
+            }
+        } else if socket.may_send() {
+            debug!("tcp:6969 close");
             socket.close();
         }
+
         phy_wait(fd, iface.poll_delay(timestamp)).expect("wait error");
     }
-
-    // 4. Connect hello world server with Store
-
 }
--- src/ohua_util/store.rs
+++ src/ohua_util/store.rs
@@ -1,13 +1,11 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::io::Write;
-use std::net::TcpStream;
-use std::sync::Arc;
+use std::str;
+use log::debug;
 
 #[derive(Clone, Default)]
 pub struct Store {
-    data: Arc<HashMap<String, HashMap<String, String>>>,
-    // modified_keys: Vec<String>,
+    data: HashMap<String, String>,
 }
 
 impl Store {
@@ -15,68 +13,61 @@ impl Store {
       //  unimplemented!()
     //}
 
-    pub fn update(&mut self, items: Vec<Option<(Message, TcpStream)>>) {
-        // -> Vec<(Message, TcpStream)> {
-        let elems = Arc::get_mut(&mut self.data).unwrap();
+    pub fn handle_message(&mut self, input_bytes:&[u8]) -> Vec<u8>{
 
-        for item in items {
-            match item {
-                Some((Message::Read(_), _)) => unreachable!(),
-                Some((Message::Write(write_msg), mut stream)) => {
-                    elems.insert(write_msg.key, write_msg.value);
+        let mut message_bytes = input_bytes.to_vec();
+        if input_bytes.ends_with(&[b'\n']) {
+            debug!("ends with linefeed");
+            message_bytes.pop();
+        }
 
-                    stream.write_all("OK".as_bytes()).unwrap();
-                    // if self.modified_keys.contains(&write_msg.key) {
-                    //     Some((Message::Write(write_msg), stream))
-                    // } else {
-                    //     self.modified_keys.push(write_msg.key.clone());
-                    //     self.data.insert(write_msg.key, write_msg.value);
-                    //     None
-                    // }
-                }
-                Some((Message::Delete(del_msg), mut stream)) => {
-                    elems.remove(&del_msg.key);
-
-                    stream.write_all("OK".as_bytes()).unwrap();
-                    // if self.modified_keys.contains(&del_msg.key) {
-                    //     Some((Message::Delete(del_msg), stream))
-                    // } else {
-                    //     self.modified_keys.push(del_msg.key.clone());
-                    //     self.data.remove(&del_msg.key);
-                    //     None
-                    // }
-                }
-                Some((Message::Update(write_msg), mut stream)) => {
-                    elems.insert(write_msg.key, write_msg.value);
-
-                    stream.write_all("OK".as_bytes()).unwrap();
-                    // if self.modified_keys.contains(&write_msg.key) {
-                    //     Some((Message::Write(write_msg), stream))
-                    // } else {
-                    //     self.modified_keys.push(write_msg.key.clone());
-                    //     self.data.insert(write_msg.key, write_msg.value);
-                    //     None
-                    // }
-                }
-                None => (), // None,
+        match serde_json::from_slice(&message_bytes) {
+            Ok(message) => {
+                let answer = self.update_state(message);
+                serde_json::to_vec(&answer).unwrap()
+            },
+            _ => {
+                let fail_msg = Answer{
+                    code:400,
+                    data: ResponseData::FailInfo("Mal formed request".to_string()) };
+                serde_json::to_vec(&fail_msg).unwrap()
             }
         }
     }
 
-    // pub fn done(&mut self) {
-    //     self.modified_keys.clear();
-    // }
-
-    pub fn get(&self, key: &str) -> Option<&HashMap<String, String>> {
-        self.data.get(key)
-    }
-
-    pub fn behappy(&self, msg:Option<Message>) -> () {
-        match msg {
-            Some(Message::Read(RequestMsg{table:_t, key:k})) => println!("Yippi {}", k),
-            _ => {}
+    fn update_state(&mut self, message:Message) -> Answer {
+            match message {
+                Message::Read(read_msg) => {
+                    if let Some(val) =
+                        self.data.get(&read_msg.key) {
+                        let rec = Record{key:read_msg.key, value: val.to_string() };
+                        Answer{code:200, data:ResponseData::Record(rec)}
+                    } else {
+                        Answer{code:400, data:ResponseData::FailInfo("No such record".to_string()) }
+                    }
+                },
+                Message::Insert(write_msg) => {
+                    if let None = self.data.get(&write_msg.key) {
+                        self.data.insert(write_msg.key, write_msg.value);
+                        Answer{code:201, data:ResponseData::Success }
+                    } else {
+                        Answer{code:400, data:ResponseData::FailInfo("Record already present".to_string()) }
+                    }
+                }
+                Message::Delete(del_msg) => {
+                    if let Some(val) = self.data.remove(&del_msg.key) {
+                        let rec = Record {key: del_msg.key, value: val};
+                        Answer { code: 200, data: ResponseData::Record(rec) };
+                    }
+                    Answer{code:400, data:ResponseData::FailInfo("Record was not present".to_string())}
+                }
+                Message::Update(write_msg) => {
+                    self.data.insert(write_msg.key, write_msg.value);
+                    Answer{code:200, data:ResponseData::Success}
+                }
+            }
         }
-    }
+
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -84,7 +75,7 @@ pub enum Message {
     /// Read a value from the key-value store
     Read(RequestMsg),
     /// Write a value.
-    Write(Record),
+    Insert(Record),
     /// Delete a value
     Delete(RequestMsg),
     /// Update a given key with the new value
@@ -93,30 +84,29 @@ pub enum Message {
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct RequestMsg {
-    pub table: String,
+    //pub table: String,
     pub key: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Record {
-    pub table: String,
+    //pub table: String,
     pub key: String,
-    pub value: HashMap<String, String>,
+    pub value: String,
 }
+
 
 #[derive(Serialize, Clone, Debug)]
-pub struct ResultRec<'a> {
-    pub table: String,
-    pub key: String,
-    pub value: &'a HashMap<String, String>,
+pub struct Answer {
+    pub code: i32,
+    pub data: ResponseData,
 }
 
-impl<'a> ResultRec<'a> {
-    pub fn from_request(req: RequestMsg, val: &'a HashMap<String, String>) -> Self {
-        Self {
-            table: req.table,
-            key: req.key,
-            value: val,
-        }
-    }
+
+#[derive(Serialize, Clone, Debug)]
+pub enum ResponseData {
+    Success,
+    FailInfo(String),
+    Record(Record),
 }
+
